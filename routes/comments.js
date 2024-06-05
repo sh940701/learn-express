906947086f4523b2f09bcdd0893fc5cc74ed79e6
sh940701 <visioner2168@gmail.com>
2024-06-05 17:21:30 +0900

    feat: comment router

--- routes/comments.js
+++ routes/comments.js
@@ -1,20 +1,45 @@
 import { Router } from 'express'
+import { MongooseComment } from '../models/mongoose/mongooseComment.js'
+import { CommentService } from '../services/commentService.js'
+import { extractCommentId } from '../middleware/parser.js'
 
 const router = Router()
 
+const commentModel = new MongooseComment()
+const commentService = new CommentService(commentModel)
+
 router
   .route('/')
-  .post((req, res, next) => {
+  .post(async (req, res) => {
     // 댓글 작성
+    try {
+      const comment = await commentService.createComment(req.body)
+      res.status(201).json(comment)
+    } catch (e) {
+      return res.status(400).json({ error: e.message })
+    }
   })
 
 router
-  .route('/:comment_id')
-  .patch((req, res, next) => {
+  .route('/:_id')
+  .patch(extractCommentId, async (req, res) => {
     // 댓글 수정
+    try {
+      const comment = await commentService.updateComment(req.commentId, req.body)
+      if (!comment) return res.status(404).json({ error: 'Comment not found' })
+      return res.status(200).json(comment)
+    } catch (e) {
+      return res.status(400).json({ error: e.message })
+    }
   })
-  .delete((req, res, next) => {
+  .delete(async (req, res) => {
     // 댓글 삭제
+    try {
+      await commentService.deleteComment(req.commentId)
+      return res.status(200).json({ message: 'Comment deleted' })
+    } catch (e) {
+      return res.status(400).json({ error: e.message })
+    }
   })
 
 
