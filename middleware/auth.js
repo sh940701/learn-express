b3e6723d23395a633b260f2225029e0951a37d33
sh940701 <visioner2168@gmail.com>
2024-06-05 22:35:12 +0900

    feat: add auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../app.js'
+
+const authenticateUser = (req, res, next) => {
+  const token = req.cookies.jwt
+
+  if (!token) {
+    return res.status(401).json({ error: '토큰이 없습니다.' })
+  }
+
+  try {
+    req.nickname = jwt.verify(token, JWT_SECRET)
+    next()
+  } catch (e) {
+    res.status(401).json({ error: '유효하지 않은 토큰입니다.' })
+  }
+}
