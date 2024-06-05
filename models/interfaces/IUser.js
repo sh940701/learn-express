1d7f4bab61bcc5a0c3fe18ce16249a7ca84deee5
sh940701 <visioner2168@gmail.com>
2024-06-05 22:34:43 +0900

    feat: user interface

---
+++ models/interfaces/IUser.js
@@ -0,0 +1,16 @@
+export class IUser {
+  constructor() {
+    // interface 로 사용하는 class 를 instance 화 할 수 없음
+    if (this.constructor === IUser) {
+      throw new Error('Cannot instantiate interface IUser')
+    }
+  }
+
+  async createUser(user) {
+    throw new Error('Method \'createUser()\' must be implemented.')
+  }
+
+  async getUser(userId) {
+    throw new Error('Method \'getUser()\' must be implemented.')
+  }
+}
