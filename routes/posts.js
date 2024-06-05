2475b6faf2ef5d6c79d7b35e1436830e4fb3ee23
sh940701 <visioner2168@gmail.com>
2024-06-05 17:01:20 +0900

    feat: post router

--- routes/posts.js
+++ routes/posts.js
@@ -1,33 +1,77 @@
 import { Router } from 'express'
+import { MongoosePost } from '../models/mongoose/mongoosePost.js'
+import { PostService } from '../services/postService.js'
+import { extractPostId } from '../middleware/parser.js'
 
 const router = Router()
 
+const postModel = new MongoosePost()
+const postService = new PostService(postModel)
+
 router
   .route('/')
-  .get((req, res, next) => {
+  .get(async (req, res) => {
     // 전체 게시글 목록 조회
-    return res.json({ msg: 'posts' })
+    try {
+      const posts = await postService.getPosts()
+
+      return res.status(200).json(posts)
+    } catch (e) {
+      res.status(500).json({ error: e.message })
+    }
   })
-  .post((req, res, next) => {
+  .post(async (req, res) => {
     // 게시글 작성
+    try {
+      const posts = await postService.createPost(res.body)
+
+      return res.status(201).json(posts)
+    } catch (e) {
+      res.status(400).json({ error: e.message })
+    }
   })
 
 router
   .route('/:_id')
-  .get((req, res, next) => {
+  .get(extractPostId, async (req, res) => {
     // 단일 게시글 조회
+    try {
+      const post = await postService.getPost(req.postId)
+
+      return res.status(200).json(post)
+    } catch (e) {
+      res.status(400).json({ error: e.message })
+    }
   })
-  .patch((req, res, next) => {
+  .patch(extractPostId, async (req, res) => {
     // 게시글 수정
+    try {
+      const post = await postService.updatePost(req.postId, req.body)
+      if (!post) return res.status(404).json({ error: 'Post not found' })
+      return res.status(200).json(post)
+    } catch (e) {
+      return res.status(404).json({ error: e.message })
+    }
   })
-  .delete((req, res, next) => {
-    // 게시글 삭제
+  .delete(extractPostId, async (req, res) => {
+    try {
+      await postService.deletePost(req.postId)
+      return res.status(200).json({ message: 'User deleted' })
+    } catch (e) {
+      return res.status(500).json({ error: e.message })
+    }
   })
 
 router
   .route(':_id/comments')
-  .get((req, rex, next) => {
-    // 게시글에 포함된 댓글들 삭제
+  .get(extractPostId, async (req, res, next) => {
+    // 게시글에 포함된 댓글들 조회
+    try {
+      await postService.deletePost(req.postId)
+      return res.status(200).json({ message: 'Post deleted' })
+    } catch (e) {
+      return res.status(500).json({ error: e.message })
+    }
   })
 
 export default router
