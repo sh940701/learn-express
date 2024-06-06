b7391b0f108e0d86b66d141d61991a0184f34a90
sh940701 <visioner2168@gmail.com>
2024-06-06 23:19:55 +0900

    update: setup sequelize

--- app.js
+++ app.js
@@ -4,20 +4,19 @@ import dotenv from 'dotenv'
 import swaggerUi from 'swagger-ui-express'
 import { swaggerSpec } from './config/swagger.js'
 import cookieParser from 'cookie-parser'
-import sequelize from './models/mysql/index.js'
+import sequelize from './config/mysql.js'
 
 const app = express()
 dotenv.config()
 
 const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-const DB_NAME = process.env.DB_NAME
+export const DB_NAME = process.env.DB_NAME
+export const dbType = process.env.DB_TYPE
 export const JWT_SECRET = process.env.JWT_SECRET
 // await connectDB(DB_CONNECTION_STRING)
 
 await sequelize.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME};`)
-await sequelize.query(`USE ${DB_NAME}`)
-await sequelize.sync({ force: true })
-
+await sequelize.sync({ force: false })
 
 app.use(express.json())
 app.use(cookieParser())
