57fec42620fdfbd59388975d8717b0b1c88887eb
sh940701 <visioner2168@gmail.com>
2024-06-07 01:25:37 +0900

    remove: unnecessary log

--- models/mysql/mysqlComment.js
+++ models/mysql/mysqlComment.js
@@ -42,7 +42,6 @@ export class MysqlComment extends IComment {
   }
 
   async updateComment(commentId, comment, nickname) {
-    console.log(commentId)
     const dbComment = await Comment.findOne({ where: { id: commentId, author: nickname } })
 
     if (!dbComment) return null
